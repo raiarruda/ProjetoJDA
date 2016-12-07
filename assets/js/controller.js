2e3f45a49e4ca4b549f769964ab805fe8a07d71d
Raissa Arruda de Araújo <Raissa Arruda de Arajo>
2016-12-07 16:23:04 -0200

    Commit by raissa

---
+++ assets/js/controller.js
@@ -0,0 +1,131 @@
+var app = angular.module('mainApp', ['ngRoute]']);
+
+app.config(function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : 'index.html'
+	}).when('/login', {
+		templateUrl : 'login2.html'
+	}).otherwise({
+		redirectTo : '/'
+	});
+
+});
+
+app.controller('loginController', function($scope, $location) {
+	$scope.submeter = function() {
+	};
+	var nomeUsuario = $scope.nomeUsuario;
+	var senha = $scope.senha;
+
+	if ($scope.nomeUsuario == 'admin' && $scope.senha == 'admin') {
+		$location.path('/');
+	}
+
+});
+
+angular.module("produtos", []);
+angular.module("produtos").controller("produtosController", function($scope) {
+
+	$scope.produtos = [{
+		image : "assets/images/thumb253-1.jpg",
+		nome : "produto1",
+		descricao : "teste1",
+		preco : "4.0",
+		categoria : "limpeza"
+	}, {
+		image : "assets/images/thumb253-2.jpg",
+		nome : "produto2",
+		descricao : "teste2",
+		preco : "4.0",
+		categoria : "limpeza"
+	}, {
+		image : "assets/images/thumb253-3.jpg",
+		nome : "produto3",
+		descricao : "teste3",
+		preco : "4.0",
+		categoria : "limpeza"
+	}, {
+		image : "assets/images/thumb253-1.jpg",
+		nome : "produto1",
+		descricao : "teste1",
+		preco : "4.0",
+		categoria : "limpeza"
+	}, {
+		image : "assets/images/thumb253-2.jpg",
+		nome : "produto2",
+		descricao : "teste2",
+		preco : "4.0",
+		categoria : "limpeza"
+	}, {
+		image : "assets/images/thumb253-3.jpg",
+		nome : "produto3",
+		descricao : "teste3",
+		preco : "4.0",
+		categoria : "limpeza"
+	}, {
+		image : "assets/images/thumb253-1.jpg",
+		nome : "produto1",
+		descricao : "teste1",
+		preco : "4.0",
+		categoria : "limpeza"
+	}, {
+		image : "assets/images/thumb253-2.jpg",
+		nome : "produto2",
+		descricao : "teste2",
+		preco : "4.0",
+		categoria : "limpeza"
+	}, {
+		image : "assets/images/thumb253-3.jpg",
+		nome : "produto3",
+		descricao : "teste3",
+		preco : "4.0",
+		categoria : "limpeza"
+	}, {
+		image : "assets/images/thumb253-4.jpg",
+		nome : "produto4",
+		descricao : "teste4",
+		preco : "4.0",
+		categoria : "limpeza"
+	}];
+	$scope.carrinho = [];
+	$scope.adicionaNoCarrinho = function(produto) {
+		$scope.carrinho.push(produto);
+	};
+
+});
+
+angular.module("usuarios", []);
+angular.module("usuarios").controller("usuariosController", function($scope) {
+	$scope.usuarios=[];
+	$scope.adicionarUsuario = function(usuario,senha) {
+
+		$scope.usuarios.push({usuario: usuario, senha:senha });
+	};
+
+});
+
+'use strict';
+ 
+angular.module('Authentication')
+ 
+.controller('LoginController',
+    ['$scope', '$rootScope', '$location', 'AuthenticationService',
+    function ($scope, $rootScope, $location, AuthenticationService) {
+        // reset login status
+        AuthenticationService.ClearCredentials();
+ 
+        $scope.login = function () {
+            $scope.dataLoading = true;
+            AuthenticationService.Login($scope.username, $scope.password, function(response) {
+                if(response.success) {
+                    AuthenticationService.SetCredentials($scope.username, $scope.password);
+                    $location.path('/');
+                } else {
+                    $scope.error = response.message;
+                    $scope.dataLoading = false;
+                }
+            });
+        };
+    }]);
+
+
