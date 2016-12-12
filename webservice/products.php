5e129f3a080b88c5e68d319540244fa10ea754b1
kainzen <koji864@hotmail.com>
2016-12-12 21:56:55 -0200

    Serviços de Login e Produtos mais o arquivo de configuração do PHP.

---
+++ webservice/products.php
@@ -0,0 +1,27 @@
+<?php
+	// Include confi.php
+	include_once('config.php');
+
+	$categoria = isset($_GET['fk_categoria']) ? mysql_real_escape_string($_GET['fk_categoria']) :  "";
+	$id = isset($_GET['id']) ? mysql_real_escape_string($_GET['id']) :  "";
+	if(empty($categoria) || empty($id)){
+		mysql_set_charset('utf8');
+		$qur = mysql_query("select `produto`.id, `categoria`.nome as categoria_nome, `produto`.nome, `produto`.descricao, `produto`.valor from `produto` inner join `categoria` on `produto`.fk_categoria = `categoria`.id");
+		$result =array();
+		while($r = mysql_fetch_array($qur)){
+			extract($r);
+			$result[] = array("id" => $id_produto, "categoria_nome" => $categoria, "nome" => $nome, "descricao" => $descricao, "valor" => $valor); 
+		}
+		if(!empty($result)) {
+			$json = array("status" => 1, "info" => $result);
+		}else{
+			$json = array("status" => 0, "msg" => "Produto not found");
+		}
+	}else{
+		$json = array("status" => 0, "msg" => "Empty fields");
+	}
+	@mysql_close($conn);
+
+	/* Output header */
+	header('Content-type: application/json');
+	echo json_encode($json);
