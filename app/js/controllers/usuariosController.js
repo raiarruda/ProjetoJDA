ec8fe924fd1d749d5114728ed7d4d5d706d2dde8
Leonan Teixeira <leonan.teixeira@gmail.com>
2016-12-22 21:22:35 -0300

    adicionado condigurações do npm

--- app/js/controllers/usuariosController.js
+++ app/js/controllers/usuariosController.js
@@ -4,15 +4,12 @@ angular.module("minimercado").controller("usuariosController", function($scope) 
 
 	$scope.cadastrarUsuario = function(isValid){
 		console.log("0");
-		
 		isValid = $scope.casdatroForm.$valid;
 		if ($scope.casdatroForm.$valid) {
 			console.log("1");
 			adicionaUsuario($scope.cadastroForm.nomeUsuario, $scope.cadastroForm.loginUsuario, $scope.cadastroForm.senhaUsuario);
 			
 		}
-		
-		
 	};
 	
 	$scope.adicionaUsuario = function(nomeUsuario, loginUsuario, senhaUsuario) {
