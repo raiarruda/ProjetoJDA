e736a70e0f7fa9abbdddc3c5a7febfee65e2ff6e
kainzen <koji864@hotmail.com>
2017-01-03 19:22:31 -0200

    Alterações nos menus de Gameplay e Missões.

--- webservice/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysql_connect("mysql11.redehost.com.br:3306", "jda2016", "Jda@201600");
-mysql_select_db('minimercado', $conn);
-
-#interno
-#mysql11.redehost.com.br:41890
-#externo
-#mysql11.redehost.com.br:3306
